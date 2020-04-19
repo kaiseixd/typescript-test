7ec35971683a94e5582de85b437ed7cefe6820d9
kaiseixd <1050207224@qq.com>
2020-04-20 00:06:37 +0800

    add react

---
+++ react/src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Hello from './demo/hello'
+
+ReactDOM.render(
+    <Hello name="TypeScript" />,
+    document.querySelectorAll('.app')[0]
+)
